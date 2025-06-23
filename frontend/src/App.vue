a62373e9eacc0c6a934c4c18f44b6c7666e6cb3b
Darksasuke911 <maceo.marlin@efrei.net>
2025-06-23 15:28:47 +0200

    Suppression de duplicatat de suggestion

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,26 @@
 <template>
-  <div>
+  <head>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  
+  <header>
     <nav>
-      <router-link to="/">Accueil</router-link> |
-      <router-link to="/ACPM">À propos</router-link>
+      <div class="nav-left">
+        <img class="nav_item" src="./assets/vue.svg"/>
+        <router-link to="/">Accueil</router-link>
+      </div>
+      <div class="nav-right">
+        <router-link to="/ACPM">Dev</router-link>
+      </div>
     </nav>
-    <router-view />
-  </div>
+    <router-view/>
+  </header>
+  <footer>
+    <div class="footer-content">
+      <p>© 2025 - Tous droits réservés</p>
+      <p>Développé par <a href="https://github.com/VeKTyS" target="_blank">VeKTyS</a>
+      | <a href="https://github.com/MaceoMARLIN" target="_blank">Darksasuke911</a>
+      | <a href="https://github.com/jona3341" target="_blank">jona3341</a></p>
+    </div>
+  </footer>
 </template>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,5 +3,9 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 import 'leaflet/dist/leaflet.css';
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import '@mdi/font/css/materialdesignicons.css'
 
-createApp(App).use(router).mount('#app')
+const vuetify = createVuetify()
+createApp(App).use(vuetify).use(router).mount('#app')
