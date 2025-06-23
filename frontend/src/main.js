5aa8604c4b569d61d2f50564e4465e47635da5c4
Darksasuke911 <maceo.marlin@efrei.net>
2025-06-23 15:39:26 +0200

    vuetify clean uninstall

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,9 +3,5 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 import 'leaflet/dist/leaflet.css';
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import '@mdi/font/css/materialdesignicons.css'
 
-const vuetify = createVuetify()
-createApp(App).use(vuetify).use(router).mount('#app')
+createApp(App).use(router).mount('#app')
