18715e064c2be23973997c2a3f49734cf85c9980
YoonA <jonathan170204@gmail.com>
2025-06-27 12:03:17 +0200

    Ajout d'un texte rdm pour ACPM

--- frontend/src/pages/ACPM.vue
+++ frontend/src/pages/ACPM.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
     <h1>À propos</h1>
-    <p>Voici la page à propos.</p>
+    <p>Bienvenue sur la page À propos de notre application révolutionnaire d’itinéraires métropolitains.
+Grâce à une synergie inédite entre intelligence artificielle, algorithmes de graphes avancés et expérience utilisateur immersive, nous redéfinissons la mobilité urbaine.
+Notre solution, pensée pour anticiper les besoins de demain, s’appuie sur une infrastructure scalable et des technologies de pointe pour offrir une optimisation dynamique des parcours, tout en garantissant une interface intuitive et responsive.
+Rejoignez-nous dans cette aventure disruptive où chaque correspondance devient une opportunité et chaque trajet une expérience augmentée.
+Parce que l’innovation ne s’arrête jamais, notre équipe s’engage à repousser les frontières de l’itinéraire intelligent, pour une ville plus connectée, fluide et durable.</p>
   </div>
 </template>
