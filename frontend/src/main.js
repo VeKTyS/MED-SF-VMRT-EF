81d9c9742cf0ec7a2a4d70018ad1c99c75a5f2c7
VeKTyS <gtastyle.731@gmail.com>
2025-07-07 15:40:00 +0200

    Ajout fonctionnalité traduction (module google trad)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,5 +3,18 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import iconUrl from 'leaflet/dist/images/marker-icon.png';
+import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png';
+import shadowUrl from 'leaflet/dist/images/marker-shadow.png';
 
-createApp(App).use(router).mount('#app')
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconUrl,
+  iconRetinaUrl,
+  shadowUrl
+});
+
+createApp(App)
+  .use(router)
+  .mount('#app');
